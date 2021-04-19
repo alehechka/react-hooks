00d2f1da2c32b9edd316ca2f59550332956c8a2a
alehechka <42357034+alehechka@users.noreply.github.com>
2021-04-19 12:41:55 -0500

    useToggle

---
+++ src/useToggle.ts
@@ -0,0 +1,23 @@
+import { useState, useCallback } from 'react';
+
+export type ToggleIndexes = [boolean, VoidFunction, VoidFunction, VoidFunction];
+
+export const useToggle = (initialState: boolean = false): ToggleIndexes => {
+	const [value, setValue] = useState<boolean>(initialState);
+
+	const toggle = useCallback(() => {
+		setValue((v) => !v);
+	}, []);
+
+	const turnOff = useCallback(() => {
+		setValue(false);
+	}, []);
+
+	const turnOn = useCallback(() => {
+		setValue(true);
+	}, []);
+
+	return [value, toggle, turnOn, turnOff];
+};
+
+export default useToggle;
