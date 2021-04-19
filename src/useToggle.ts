4769a6544bbd80ac4e134a18e0bf2e006a762d84
alehechka <42357034+alehechka@users.noreply.github.com>
2021-04-19 13:19:00 -0500

    Resolve Linting x2

--- src/useToggle.ts
+++ src/useToggle.ts
@@ -3,21 +3,21 @@ import { useState, useCallback } from 'react';
 export type ToggleIndexes = [boolean, VoidFunction, VoidFunction, VoidFunction];
 
 export const useToggle = (initialState: boolean = false): ToggleIndexes => {
-	const [value, setValue] = useState<boolean>(initialState);
+  const [value, setValue] = useState<boolean>(initialState);
 
-	const toggle = useCallback(() => {
-		setValue((v) => !v);
-	}, []);
+  const toggle = useCallback(() => {
+    setValue(v => !v);
+  }, []);
 
-	const turnOff = useCallback(() => {
-		setValue(false);
-	}, []);
+  const turnOff = useCallback(() => {
+    setValue(false);
+  }, []);
 
-	const turnOn = useCallback(() => {
-		setValue(true);
-	}, []);
+  const turnOn = useCallback(() => {
+    setValue(true);
+  }, []);
 
-	return [value, toggle, turnOn, turnOff];
+  return [value, toggle, turnOn, turnOff];
 };
 
 export default useToggle;
